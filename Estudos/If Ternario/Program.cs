653b264b0c9b908a75b1886032dc7cf2bc0038c0
Paulo Henrique <96488452+PauloHPDS@users.noreply.github.com>
2024-01-22 23:39:19 -0300

    Estudos em C#

---
+++ Estudos/If Ternario/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Contracts;
+
+//// IF Ternario
+
+int n = 20;
+bool ePar=false;
+
+//if ternario
+ePar= n % 2 == 0;
+System.Console.WriteLine($"{n} é "+(ePar ? "par":"impar"));
+
+//if normal
+
+if (n%2==0){
+    System.Console.WriteLine($"{n} é par");
+}else{
+    System.Console.WriteLine($"{n} é impar");
+}
+
